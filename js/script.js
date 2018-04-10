7980ece503b36078fd581161a827491ed168e564
Dominik Kolodziej <kolodziej.dominik@gmail.com>
2018-04-10 16:16:17 +0200

    Fix bug with missing semicolons in js/script.js

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ function Phone(brand, price, color) {
 }
 Phone.prototype.printInfo = function() {
 	console.log("The phone brand is " + this.brand + ", color is " + this.color + " and the price is " + this.price + ".");
-}
+};
 var iPhone6s = new Phone("Apple", 2069, "silver"),
 	samsungGalaxyS6 = new Phone("Samsung", 1199, "black"),
 	onePlusOne = new Phone("OnePlus", 1299, "black");
@@ -16,7 +16,7 @@ onePlusOne.printInfo();
 
 Phone.prototype.price = function () {
 	alert("The lowest price for " + this.color + " " + this.brand + ", given by the price comparision engine is " + this.price + ".");
-}
+};
 
 iPhone6s.price();
 samsungGalaxyS6.price();
