45e5f899a576b352389c3ff488a9adff3a73d564
Dominik Kolodziej <kolodziej.dominik@gmail.com>
2018-04-10 18:44:24 +0200

    Correct code formatting

--- js/script.js
+++ js/script.js
@@ -6,6 +6,10 @@ function Phone(brand, price, color) {
 Phone.prototype.printInfo = function() {
 	console.log("The phone brand is " + this.brand + ", color is " + this.color + " and the price is " + this.price + ".");
 };
+Phone.prototype.price = function() {
+	alert("The lowest price for " + this.color + " " + this.brand + " given by the price comparision engine is " + this.price + ".");
+};
+
 var iPhone6s = new Phone("Apple", 2069, "silver"),
 	samsungGalaxyS6 = new Phone("Samsung", 1199, "black"),
 	onePlusOne = new Phone("OnePlus", 1299, "black");
@@ -13,10 +17,6 @@ var iPhone6s = new Phone("Apple", 2069, "silver"),
 iPhone6s.printInfo();
 samsungGalaxyS6.printInfo();
 onePlusOne.printInfo();
-
-Phone.prototype.price = function () {
-	alert("The lowest price for " + this.color + " " + this.brand + ", given by the price comparision engine is " + this.price + ".");
-};
 
 iPhone6s.price();
 samsungGalaxyS6.price();
