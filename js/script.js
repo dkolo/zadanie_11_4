873db6b4234855f5748d07ab1b4a4393d88d7cbc
Dominik Kolodziej <kolodziej.dominik@gmail.com>
2018-04-10 16:12:40 +0200

    Add new function created with prototype which alerts lowest price of phones

--- js/script.js
+++ js/script.js
@@ -4,12 +4,20 @@ function Phone(brand, price, color) {
 	this.color = color;
 }
 Phone.prototype.printInfo = function() {
-		console.log("The phone brand is " + this.brand + ", color is " + this.color + " and the price is " + this.price + ".");
+	console.log("The phone brand is " + this.brand + ", color is " + this.color + " and the price is " + this.price + ".");
 }
-var iPhone6s = new Phone("Apple", 2250, "silver"),
+var iPhone6s = new Phone("Apple", 2069, "silver"),
 	samsungGalaxyS6 = new Phone("Samsung", 1199, "black"),
 	onePlusOne = new Phone("OnePlus", 1299, "black");
 
 iPhone6s.printInfo();
 samsungGalaxyS6.printInfo();
 onePlusOne.printInfo();
+
+Phone.prototype.price = function () {
+	alert("The lowest price for " + this.color + " " + this.brand + ", given by the price comparision engine is " + this.price + ".");
+}
+
+iPhone6s.price();
+samsungGalaxyS6.price();
+onePlusOne.price();
