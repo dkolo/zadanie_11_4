68f422e7c4ba5e612bf613e8a85e9692878b8931
Dominik Kolodziej <kolodziej.dominik@gmail.com>
2018-04-10 16:00:48 +0200

    Add 3 new variables defined by function Phone

--- js/script.js
+++ js/script.js
@@ -6,3 +6,7 @@ function Phone(brand, price, color) {
 Phone.prototype.printInfo = function() {
 		console.log("The phone brand is " + this.brand + ", color is " + this.color + " and the price is " + this.price + ".");
 }
+var iPhone6s = new Phone("Apple", 2250, "silver"),
+	samsungGalaxyS6 = new Phone("Samsung", 1199, "black"),
+	onePlusOne = new Phone("OnePlus", 1299, "black");
+
